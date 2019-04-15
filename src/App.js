694d3620c67b981b53c1992bc92a4935bb030e1f
Paddy McCall <pmccall@spotx.tv>
2019-04-15 22:25:43 +0100

    Landing page update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import logo from './silcon-systems-logo.png';
 import './App.css';
 
-// const phoneNumber = "02887723861";
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Our new site is under construction. In the mean time...
-          </p>
+          <h1>
+            Our new site is under construction.
+          </h1>
+          <h3>
+            In the mean time you can ...
+          </h3>
           <div id="contact">
             <h3>
               <a href="mailto:silcon@btconnect">email us</a> | <a href="tel:+442887723861" > call us </a>
