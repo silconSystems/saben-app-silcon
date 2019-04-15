ec2ad252570b917e322b7a7341be213c1cb2ceb0
Paddy McCall <pmccall@spotx.tv>
2019-04-15 22:08:44 +0100

    Updates

---
+++ precache-manifest.fb36cd8f0f00efe04271ace5dfecdcdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00e1e4969fdd305a8b123dcbf2249a66",
+    "url": "/saben-app-silcon/static/media/silcon-systems-logo.00e1e496.png"
+  },
+  {
+    "revision": "23fca7c8e3b969121852",
+    "url": "/saben-app-silcon/static/js/runtime~main.99da6b0c.js"
+  },
+  {
+    "revision": "2f195c1c917dd1d67f7b",
+    "url": "/saben-app-silcon/static/js/main.52ea796e.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/saben-app-silcon/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "2f195c1c917dd1d67f7b",
+    "url": "/saben-app-silcon/static/css/main.42c40981.chunk.css"
+  },
+  {
+    "revision": "6e9ee3d910482481006e909c065c1838",
+    "url": "/saben-app-silcon/index.html"
+  }
+];
