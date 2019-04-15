900eacbec59c457b8a2021c9c14240b570c3a596
Paddy McCall <pmccall@spotx.tv>
2019-04-15 21:52:24 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './silcon-systems-logo.png';
 import './App.css';
+
+// const phoneNumber = "02887723861";
 
 class App extends Component {
   render() {
@@ -9,16 +11,13 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Our new site is under construction. In the mean time...
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div id="contact">
+            <h3>
+              <a href="mailto:silcon@btconnect">email us</a> | <a href="tel:+442887723861" > call us </a>
+            </h3>
+          </div>
         </header>
       </div>
     );
